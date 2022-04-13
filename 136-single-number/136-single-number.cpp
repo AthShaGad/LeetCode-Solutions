a05867db6aba4aaf800c2ed040acf268050b2b50
Atharva Shailesh Gadad <79466218+AthShaGad@users.noreply.github.com>
2022-04-13 21:26:35 +0530

    Time: 43 ms (14.84%), Space: 20.8 MB (5.11%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        //Simple approach
-        for(int i=1;i<nums.size();i++)
-            nums[0]^=nums[i];// XOR
-        return nums[0];
-        // set<int> set1;
-        // for(int i:nums)
-        //     set1.insert(i);
-        // int sum1=0, sum2=0;
-        // for(int i:set1)
-        //     sum1+=2*i;
-        // for(int i:nums)
-        //     sum2+=i;
-        // return sum1-sum2;
+        // //Simple approach
+        // for(int i=1;i<nums.size();i++)
+        //     nums[0]^=nums[i];// XOR
+        // return nums[0];
+        set<int> set1;
+        for(int i:nums)
+            set1.insert(i);
+        int sum1=0, sum2=0;
+        for(int i:set1)
+            sum1+=2*i;
+        for(int i:nums)
+            sum2+=i;
+        return sum1-sum2;
     }
 };
